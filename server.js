24f04a0c542e0a26cf1905ebb06768bb02406622
Jacob Cacciamani <cacciamani10@gmail.com>
2020-01-31 01:49:24 -0500

    added cors package to handle requests

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const PORT = process.env.PORT || 5000;
 const fs = require('fs');
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
